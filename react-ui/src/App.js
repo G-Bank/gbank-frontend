39b0503806e7efe9b435bebfb760256317759d0c
perriex <parna80as@gmail.com>
2023-07-10 19:39:26 +0330

    remove unnecessary items

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,19 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline, StyledEngineProvider } from '@material-ui/core';
-
-// routing
 import Routes from './routes';
-
-// defaultTheme
 import theme from './themes';
-
-// project imports
 import NavigationScroll from './layout/NavigationScroll';
 
-//-----------------------|| APP ||-----------------------//
 
 const App = () => {
     const customization = useSelector((state) => state.customization);
