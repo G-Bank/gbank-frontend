4080cee217e406cdd8c866e316eca2142e725436
perriex <parna80as@gmail.com>
2023-07-15 11:01:18 +0330

    add yml for page

---
+++ src/config.js
@@ -0,0 +1,17 @@
+let BACKEND_SERVER = null;
+if (process.env.REACT_APP_BACKEND_SERVER) {
+  BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
+} else {
+  BACKEND_SERVER = 'http://localhost:8000/api/';
+}
+
+const config = {
+  basename: '/gbank-frontend',
+  defaultPath: '/dashboard',
+  fontFamily: `'Vazir', 'Roboto', sans-serif`,
+  borderRadius: 12,
+  API_SERVER: BACKEND_SERVER,
+  direction: 'ltr'
+};
+
+export default config;
