39b0c4af108d566d8ae4ae5b1f190b988d657eb1
perriex <parna80as@gmail.com>
2023-08-07 10:48:24 +0330

    [UPDATE] api addr

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let BACKEND_SERVER = null;
 if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
-  BACKEND_SERVER = 'https://5e2bf869-8829-46e7-83f6-aa63b96475f7.mock.pstmn.io/api/';
+  BACKEND_SERVER = 'https://6c1af5f2-d5aa-4716-a598-866c5394b077.mock.pstmn.io/api/';
 }
 
 const config = {
