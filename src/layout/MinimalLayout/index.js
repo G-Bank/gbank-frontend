0da5cd35dc530c1e4499f6034252fd5cc5e1b852
Mahdi Efani <mahdi.erfani@tap30.net>
2023-09-12 06:55:15 +0330

    background color layout

--- src/layout/MinimalLayout/index.js
+++ src/layout/MinimalLayout/index.js
@@ -6,7 +6,7 @@ import React from 'react';
 
 const MinimalLayout = (props) => {
   return (
-    <Box mx='auto' width="100%" height="100%" maxWidth={440} bgcolor="#e7e7e7" padding={2}>
+    <Box mx='auto' width="100%" height="100%" maxWidth={440} padding={2}>
       {props.children}
       <Box width='100%' height='5em' />
     </Box>
