2e7848b7b109a4b90cb066b40bd6741966d3ff64
Mahdi Efani <mahdi.erfani@tap30.net>
2023-09-18 06:45:00 +0330

    log out user on token expire

--- src/api/server.js
+++ src/api/server.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
 import config from '../config';
 import { store } from '../store';
+import { logoutUser } from './user';
 
-export const get = (endpoint, params) => {
-  const token = store.getState().account?.token;
-  return axios.get(`${config.API_SERVER}${endpoint}`, { params, headers: { Authorization: `Token ${token}` } });
+export const get = async (endpoint, params) => {
+  try {
+    const token = store.getState().account?.token;
+    return await axios.get(`${config.API_SERVER}${endpoint}`, { params, headers: { Authorization: `Token ${token}` } });
+  } catch (err) {
+    if (err.response.status === 401) {
+      logoutUser();
+    }
+  }
 };
 
-export const post = (endpoint, body, params, contentType) => {
-  const token = store.getState().account?.token;
-  return axios.post(`${config.API_SERVER}${endpoint}`, body, {
-    params,
-    headers: { Authorization: token && `Token ${token}`, 'Content-type': contentType || 'application/json' }
-  });
+export const post = async (endpoint, body, params, contentType) => {
+  try {
+      const token = store.getState().account?.token;
+      return await axios.post(`${config.API_SERVER}${endpoint}`, body, {
+        params,
+        headers: { Authorization: token && `Token ${token}`, 'Content-type': contentType || 'application/json' }
+      });
+  } catch (err) {
+    if (err.response.status === 401) {
+      logoutUser();
+    }
+  }
 };
