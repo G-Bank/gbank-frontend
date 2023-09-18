ea6777ce021aec5a86c07d64684e269107671340
Mahdi Efani <mahdi.erfani@tap30.net>
2023-09-18 06:49:43 +0330

    provide hash routre

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persister } from './store';
@@ -12,9 +12,9 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persister}>
-      <BrowserRouter basename={config.basename}>
+      <HashRouter basename={config.basename}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
